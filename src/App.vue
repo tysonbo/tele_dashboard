38714d6ed6285cf88d6ee4d2b9f8589bf8dcca68
Jason_Grucza@Progressive.com <Jason_Grucza@Progressive.com>
2018-10-24 15:33:46 -0400

    updated to point to template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">    
-    <div class="container">     
+    <div>     
     <main>
       <router-view/>
     </main>
@@ -21,11 +21,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
+  padding: 0px;
 }
 </style>
